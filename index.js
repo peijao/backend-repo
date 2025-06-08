2bfbce8bb8d2a909170938bd7182eb48a45f2aa8
admin <admin@admins-MacBook-Pro-2.local>
2025-06-08 14:05:46 +0300

    Initial backend commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// Настройка SMTP Zoho Europe
+const transporter = nodemailer.createTransport({
+  host: "smtp.zoho.eu",
+  port: 587, // или 465 с secure: true
+  secure: false, // TLS
+  auth: {
+    user: "sait@ageinvest.am",
+    pass: "552065Gor!", // пароль от ящика или пароль приложения (если 2FA)
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+// Обработка POST-запроса с формы
+app.post("/send", (req, res) => {
+  const { formName, formPhone, formMessage } = req.body;
+
+  const mailOptions = {
+    from: "sait@ageinvest.am",
+    to: "info@ageinvest.am",
+    subject: `Новое сообщение от ${formName}`,
+    text: `Имя: ${formName}\nТелефон: ${formPhone}\nСообщение: ${formMessage}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Ошибка отправки почты:", error);
+      return res.status(500).json({ message: "Ошибка при отправке письма" });
+    }
+    console.log("Письмо успешно отправлено:", info.response);
+    res.json({ message: "Сообщение отправлено!" });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server запущен на порту ${PORT}`);
+});
